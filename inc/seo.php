87213791def1a4fea1eb0a1d2795507f41a86050
Seaton Jiang <seaton@vtrois.com>
2017-12-12 11:00:12 +0800

    update seo functions

---
+++ inc/seo.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+* Nofollow the comments and images link
+*
+* @author Vtrois <seaton@vtrois.com>
+* @license GPL-3.0
+* @since 0.1.2
+*/
+add_filter( 'comments_popup_link_attributes', 'nofollow_comments_link' );
+function nofollow_comments_link() {
+	return' rel="nofollow"';
+}
+add_filter( 'the_content', 'nofollow_images_link' );
+function nofollow_images_link( $content ) {
+    $regexp = "<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>";
+    if(preg_match_all("/$regexp/siU", $content, $matches, PREG_SET_ORDER)) {
+        if( !empty($matches) ) {
+            $srcUrl = get_option('siteurl');
+            for ($i=0; $i < count($matches); $i++)
+            {
+                $tag = $matches[$i][0];
+                $tag2 = $matches[$i][0];
+                $url = $matches[$i][0];
+                $noFollow = '';
+                $pattern = '/target\s*=\s*"\s*_blank\s*"/';
+                preg_match($pattern, $tag2, $match, PREG_OFFSET_CAPTURE);
+                if( count($match) < 1 )
+                    $noFollow .= ' target="_blank" ';
+                $pattern = '/rel\s*=\s*"\s*[n|d]ofollow\s*"/';
+                preg_match($pattern, $tag2, $match, PREG_OFFSET_CAPTURE);
+                if( count($match) < 1 )
+                    $noFollow .= ' rel="nofollow" ';
+                $pos = strpos($url,$srcUrl);
+                if ($pos === false) {
+                    $tag = rtrim ($tag,'>');
+                    $tag .= $noFollow.'>';
+                    $content = str_replace($tag2,$tag,$content);
+                }
+            }
+        }
+    }
+    $content = str_replace(']]>', ']]>', $content);
+    return $content;
+}
