57408753e6d8bc519c940242cfe4b6ac51363702
Seaton Jiang <seaton@vtrois.com>
2017-12-15 11:01:33 +0800

    Add some new require

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
-define( 'DOBBY_VERSION', '0.1.1' );
+define( 'DOBBY_VERSION', '0.1.2' );
 
 require_once( get_template_directory() . '/inc/core.php');
+require_once( get_template_directory() . '/inc/seo.php');
+require_once( get_template_directory() . '/inc/single.php');
