579c1ff46d3d345bdf48d99b4b4ba54857dbb654
Seaton Jiang <seaton@vtrois.com>
2018-02-07 19:11:17 +0800

    Add the image functions

--- inc/images.php
+++ inc/images.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
-* Get share pictures url
-*
-* @author Vtrois <seaton@vtrois.com>
-* @license GPL-3.0
-* @since 0.1.3
-* @return string url
-* @global dobby_option('image_share_default')
-*/
-function dobby_share_image(){
+ * Get the pictures url
+ *
+ * @author Vtrois <seaton@vtrois.com>
+ * @license GPL-3.0
+ * @since 1.0
+ * @return string url
+ * @global dobby_option('image_thumbnail_inside'), dobby_option('image_thumbnail_share')
+ */
+function dobby_thumbnail_url(){
     global $post;
     if (has_post_thumbnail($post->ID)) {
         $post_thumbnail_id = get_post_thumbnail_id( $post );
@@ -21,32 +21,33 @@ function dobby_share_image(){
         if (!empty($strResult[1])) {
             $img = $strResult[1][0];
         }else{
-            $img = dobby_option('image_default_share');
+          if (is_category('document')) {
+            $img = dobby_option('image_thumbnail_document');
+          } else {
+            $img = dobby_option('image_thumbnail_inside');
+          }
         }
     };
-    if (is_home()){
-        $img = dobby_option('image_default_share');
-    }
     return $img;
 }
 
 /**
-* Get post thumbnail
-*
-* @author Vtrois <seaton@vtrois.com>
-* @license GPL-3.0
-* @since 0.1.3
-* @global dobby_option('image_default_thumbnail')
-*/
+ * Get post thumbnail
+ *
+ * @author Vtrois <seaton@vtrois.com>
+ * @license GPL-3.0
+ * @since 1.0
+ * @global dobby_option('image_thumbnail_index')
+ */
 add_theme_support( 'post-thumbnails' );
 
-function dobby_thumbnail_image() {
+function dobby_thumbnail() {
   global $post;
   $img_id = get_post_thumbnail_id();
   $img_url = wp_get_attachment_image_src($img_id,'dobby-entry-thumb');
   $img_url = $img_url[0];
   if ( has_post_thumbnail() ) {
-    echo '<a href="'.get_permalink().'"><img class="img-thumbnail" src="'.$img_url.'" alt="'.get_the_title().'" /></a>';
+    echo '<a href="'.get_permalink().'"><div class="thumbnail" style="background-image: url('.$img_url.')"></div></a>';
   } else {
     $content = $post->post_content;
     $img_preg = "/<img (.*?) src=\"(.+?)\".*?>/";
@@ -55,9 +56,9 @@ function dobby_thumbnail_image() {
     if (isset($img_src[$img_count]))
       $img_val = $img_src[$img_count];
     if(!empty($img_val)){
-      echo '<a href="'.get_permalink().'"><img class="img-thumbnail" src="'.$img_val.'" alt="'.get_the_title().'" /></a>';
+      echo '<a href="'.get_permalink().'"><div class="thumbnail" style="background-image: url('.$img_val.')"></div></a>';
     } else {
-      echo '<a href="'.get_permalink().'"><img class="img-thumbnail" src="'. dobby_option('image_default_thumbnail') .'" alt="'.get_the_title().'" /></a>';
+      echo '<a href="'.get_permalink().'"><div class="thumbnail" style="background-image: url('. dobby_option('image_thumbnail_index') .')"></div></a>';
     }
   }  
 }
