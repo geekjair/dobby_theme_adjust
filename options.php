304cb067414ead39bda487f8ae0a62a677191844
Seaton Jiang <seaton@vtrois.com>
2018-02-19 19:16:33 +0800

    Update the options page

--- options.php
+++ options.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * The template for optionsframework option page
+ *
+ * @author Vtrois <seaton@vtrois.com>
+ * @license GPL-3.0
+ * @since 1.0
+ */
 function optionsframework_option_name() {
 	$themename = wp_get_theme();
 	$themename = preg_replace("/\W/", "_", strtolower($themename) );
@@ -10,32 +17,35 @@ function optionsframework_option_name() {
 
 function optionsframework_options() {
 
-	$background_defaults = array(
-		'color' => '',
-		'image' => '',
-		'repeat' => 'repeat',
-		'position' => 'top center',
-		'attachment'=>'scroll' );
-
-	$imagepath =  get_template_directory_uri() . '/images/';
+	$imagepath =  get_template_directory_uri() . '/inc/options/images/';
 
 	$options = array();
 
 	$options[] = array(
-		'name' => __( 'Global config' , 'dobby' ),
+		'name' => __( 'Global Config' , 'dobby' ),
 		'type' => 'heading');
 
 	$options[] = array(
-		'name' => __( 'Example Background', 'dobby' ),
-		'desc' => __( 'Change the background CSS.', 'dobby' ),
-		'id' => 'example_background',
-		'std' => $background_defaults,
-		'type' => 'background'
-	);
+		'name' => __( 'Navbar layout' , 'dobby' ),
+		'id' => "global_nav_layout",
+		'std' => "alpha",
+		'type' => "select",
+		'class' => 'mini',
+		'options' => array(
+			'alpha' => __( 'Style 1', 'dobby' ),
+			'gamma' => __( 'Style 2', 'dobby' ),
+	));	
 
 	$options[] = array(
 		'name' => __( 'Favicon ico' , 'dobby' ),
 		'id' => 'global_ico',
+		'type' => 'upload'
+	);
+
+	$options[] = array(
+		'name' => __( 'Default gravatar' , 'dobby' ),
+		'id' => 'image_default_gravatar',
+		'std' => get_template_directory_uri() . '/static/images/default/gravatar.png',
 		'type' => 'upload'
 	);
 
@@ -52,49 +62,355 @@ function optionsframework_options() {
 	);
 
 	$options[] = array(
-		'name' => __( 'Image config' , 'dobby' ),
+		'name' => __( 'Login page url' , 'dobby' ),
+		'id' => 'global_login',
+		'type' => 'text'
+	);	
+
+	$options[] = array(
+		'name' => __( 'ICP number' , 'dobby' ),
+		'id' => 'footer_icp_num',
+		'type' => 'text'
+	);
+
+	$options[] = array(
+		'name' => __( 'GA number' , 'dobby' ),
+		'id' => 'footer_gov_num',
+		'type' => 'text'
+	);
+
+	$options[] = array(
+		'name' => __( 'GA link' , 'dobby' ),
+		'id' => 'footer_gov_link',
+		'type' => 'text'
+	);
+
+	$options[] = array(
+		'name' => __( 'Index Config' , 'dobby' ),
 		'type' => 'heading');
 
 	$options[] = array(
-		'name' => __( 'Featured picture' , 'dobby' ),
-		'id' => 'image_default_thumbnail',
-		'std' => get_template_directory_uri() . '/images/default/thumbnail.png',
+		'name' => __( 'Function status' , 'dobby' ),
+		'desc' => __( 'Whether to enable the index page?' , 'dobby' ),
+		'id' => 'index_status',
+		'std' => '0',
+		'type' => 'checkbox');
+
+	$options[] = array(
+		'name' => __( 'Project name' , 'dobby' ),
+		'id' => 'index_project_1',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'Project meta' , 'dobby' ),
+		'id' => 'index_meta_1',
+		'type' => 'textarea');
+
+	$options[] = array(
+		'name' => __( 'Project single' , 'dobby' ),
+		'id' => 'index_single_1',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'Project name' , 'dobby' ),
+		'id' => 'index_project_2',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'Project meta' , 'dobby' ),
+		'id' => 'index_meta_2',
+		'type' => 'textarea');
+
+	$options[] = array(
+		'name' => __( 'Project category' , 'dobby' ),
+		'id' => 'index_category_2',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'Project name' , 'dobby' ),
+		'id' => 'index_project_3',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'Project meta' , 'dobby' ),
+		'id' => 'index_meta_3',
+		'type' => 'textarea');
+
+	$options[] = array(
+		'name' => __( 'Project category' , 'dobby' ),
+		'id' => 'index_category_3',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'Image Config' , 'dobby' ),
+		'type' => 'heading');
+
+	$options[] = array(
+		'name' => __( 'Category banner' , 'dobby' ),
+		'id' => 'image_default_category',
+		'std' => get_template_directory_uri() . '/static/images/default/banner.png',
 		'type' => 'upload'
 	);
 
 	$options[] = array(
-		'name' => __( 'Default gravatar' , 'dobby' ),
-		'id' => 'image_default_gravatar',
-		'std' => get_template_directory_uri() . '/images/default/gravatar.png',
+		'name' => __( 'Tag banner' , 'dobby' ),
+		'id' => 'image_default_tag',
+		'std' => get_template_directory_uri() . '/static/images/default/banner.png',
 		'type' => 'upload'
 	);
 
 	$options[] = array(
-		'name' => __( 'Share picture' , 'dobby' ),
-		'id' => 'iimage-default-share',
-		'std' => get_template_directory_uri() . '/images/default/share.png',
+		'name' => __( 'Search banner' , 'dobby' ),
+		'id' => 'image_default_search',
+		'std' => get_template_directory_uri() . '/static/images/default/banner.png',
 		'type' => 'upload'
 	);
 
 	$options[] = array(
-		'name' => __( 'Single config' , 'dobby' ),
+		'name' => __( 'Default share images' , 'dobby' ),
+		'id' => 'image_default_share',
+		'std' => get_template_directory_uri() . '/static/images/default/thumbnail.png',
+		'type' => 'upload'
+	);
+
+	$options[] = array(
+		'name' => __( '404 images' , 'dobby' ),
+		'id' => 'image_default_404',
+		'std' => get_template_directory_uri() . '/static/images/default/404.png',
+		'type' => 'upload'
+	);
+
+	$options[] = array(
+		'name' => __( 'Single Config' , 'dobby' ),
 		'type' => 'heading');
 
 	$options[] = array(
-		'name' => __( 'Page config' , 'dobby' ),
+		'name' => __( 'Single layout' , 'dobby' ),
+		'id' => "single_layout",
+		'std' => "alpha",
+		'type' => "select",
+		'class' => 'mini',
+		'options' => array(
+			'alpha' => __( 'Style 1', 'dobby' ),
+			'gamma' => __( 'Style 2', 'dobby' ),
+	));	
+
+	$options[] = array(
+		'name' => __( 'Index thumbnail' , 'dobby' ),
+		'id' => 'image_thumbnail_index',
+		'std' => get_template_directory_uri() . '/static/images/default/thumbnail.png',
+		'type' => 'upload'
+	);
+
+	$options[] = array(
+		'name' => __( 'Inside thumbnail' , 'dobby' ),
+		'id' => 'image_thumbnail_inside',
+		'std' => get_template_directory_uri() . '/static/images/default/thumbnail.png',
+		'type' => 'upload'
+	);
+
+	$options[] = array(
+		'name' => __( 'Copyright content' , 'dobby' ),
+		'id' => 'single_copyright',
+		'type' => 'text');
+	
+	$options[] = array(
+		'name' => __( 'Mail Config' , 'dobby' ),
 		'type' => 'heading');
 
 	$options[] = array(
-		'name' => __( 'Mail config' , 'dobby' ),
+		'name' => __( 'SMTP service' , 'dobby' ),
+		'desc' => __( 'Whether to enable SMTP service?' , 'dobby' ),
+		'id' => 'mail_smtps',
+		'std' => '0',
+		'type' => 'checkbox');
+
+	$options[] = array(
+		'name' => __( 'Sender' , 'dobby' ),
+		'id' => 'mail_name',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'Mail server' , 'dobby' ),
+		'id' => 'mail_host',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'Server port' , 'dobby' ),
+		'id' => 'mail_port',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'Mail account' , 'dobby' ),
+		'id' => 'mail_username',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'Email password' , 'dobby' ),
+		'id' => 'mail_passwd',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'Enable SMTPAuth service' , 'dobby' ),
+		'desc' => __( 'Whether to enable SMTPAuth service?' , 'dobby' ),
+		'id' => 'mail_smtpauth',
+		'std' => '1',
+		'type' => 'checkbox');
+
+	$options[] = array(
+		'name' => __( 'SMTPSecure set' , 'dobby' ),
+		'id' => 'mail_smtpsecure',
+		'std' => 'ssl',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'Carousel Config' , 'dobby' ),
 		'type' => 'heading');
 
 	$options[] = array(
-		'name' => __( 'Donate config' , 'dobby' ),
+		'name' => __( 'Function status' , 'dobby' ),
+		'desc' => __( 'Whether to enable the carousel?' , 'dobby' ),
+		'id' => 'carousel_status',
+		'std' => '0',
+		'type' => 'checkbox');
+
+	$options[] = array(
+		'name' => __( 'The first image' , 'dobby' ),
+		'id' => 'carousel_img_1',
+		'type' => 'upload');
+
+	$options[] = array(
+		'name' => __( 'The first url' , 'dobby' ),
+		'id' => 'carousel_url_1',
+		'std' => '',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'The first title' , 'dobby' ),
+		'id' => 'carousel_title_1',
+		'std' => '',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'The first meta' , 'dobby' ),
+		'id' => 'carousel_meta_1',
+		'std' => '',
+		'type' => 'textarea');
+
+	$options[] = array(
+		'name' => __( 'The second image' , 'dobby' ),
+		'id' => 'carousel_img_2',
+		'type' => 'upload');
+
+	$options[] = array(
+		'name' => __( 'The second url' , 'dobby' ),
+		'id' => 'carousel_url_2',
+		'std' => '',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'The second title' , 'dobby' ),
+		'id' => 'carousel_title_2',
+		'std' => '',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'The second meta' , 'dobby' ),
+		'id' => 'carousel_meta_2',
+		'std' => '',
+		'type' => 'textarea');
+
+	$options[] = array(
+		'name' => __( 'The third image' , 'dobby' ),
+		'id' => 'carousel_img_3',
+		'type' => 'upload');
+
+	$options[] = array(
+		'name' => __( 'The third url' , 'dobby' ),
+		'id' => 'carousel_url_3',
+		'std' => '',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'The third title' , 'dobby' ),
+		'id' => 'carousel_title_3',
+		'std' => '',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'The third meta' , 'dobby' ),
+		'id' => 'carousel_meta_3',
+		'std' => '',
+		'type' => 'textarea');
+
+	$options[] = array(
+		'name' => __( 'The fourth image' , 'dobby' ),
+		'id' => 'carousel_img_4',
+		'type' => 'upload');
+
+	$options[] = array(
+		'name' => __( 'The fourth url' , 'dobby' ),
+		'id' => 'carousel_url_4',
+		'std' => '',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'The fourth title' , 'dobby' ),
+		'id' => 'carousel_title_4',
+		'std' => '',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'The fourth meta' , 'dobby' ),
+		'id' => 'carousel_meta_4',
+		'std' => '',
+		'type' => 'textarea');
+
+	$options[] = array(
+		'name' => __( 'The fifth image' , 'dobby' ),
+		'id' => 'carousel_img_5',
+		'type' => 'upload');
+
+	$options[] = array(
+		'name' => __( 'The fifth url' , 'dobby' ),
+		'id' => 'carousel_url_5',
+		'std' => '',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'The fifth title' , 'dobby' ),
+		'id' => 'carousel_title_5',
+		'std' => '',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __( 'The fifth meta' , 'dobby' ),
+		'id' => 'carousel_meta_5',
+		'std' => '',
+		'type' => 'textarea');
+
+	$options[] = array(
+		'name' => __( 'Donate Config' , 'dobby' ),
 		'type' => 'heading');
 
 	$options[] = array(
-		'name' => __( 'Social config' , 'dobby' ),
-		'type' => 'heading');
+		'name' => __( 'Function status' , 'dobby' ),
+		'desc' => __( 'Whether to enable the donate?' , 'dobby' ),
+		'id' => 'donate_status',
+		'std' => '0',
+		'type' => 'checkbox');
+
+	$options[] = array(
+		'name' => __( 'Alipay QR Code' , 'dobby' ),
+		'id' => 'donate_alipay_qr',
+		'std' => get_template_directory_uri() . '/static/images/default/qr.png',
+		'type' => 'upload');
+
+	$options[] = array(
+		'name' => __( 'Wechat QR Code' , 'dobby' ),
+		'id' => 'donate_wechat_qr',
+		'std' => get_template_directory_uri() . '/static/images/default/qr.png',
+		'type' => 'upload');
 
 	return $options;
 }
