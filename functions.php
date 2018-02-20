2704f40f4e3b6166c211a40b0edcad21a0cba920
Seaton Jiang <seaton@vtrois.com>
2018-02-20 19:14:55 +0800

    Add the core functions

--- functions.php
+++ functions.php
@@ -1,8 +1,13 @@
 <?php
 
-define( 'DOBBY_VERSION', '0.1.3' );
+define( 'DOBBY_VERSION', '1.0' );
 
 require_once( get_template_directory() . '/inc/core.php');
+require_once( get_template_directory() . '/inc/smtp.php');
 require_once( get_template_directory() . '/inc/global.php');
 require_once( get_template_directory() . '/inc/images.php');
+require_once( get_template_directory() . '/inc/widget.php');
 require_once( get_template_directory() . '/inc/single.php');
+require_once( get_template_directory() . '/inc/comments.php');
+require_once( get_template_directory() . '/inc/navwalker.php');
+require_once( get_template_directory() . '/inc/shortcode.php');
